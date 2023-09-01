a86c71fd6ae0df129a40f5fb5755af7821dd5c68
Dhanish2603 <dhanishpatel.200410107076@svitvasad.ac.in>
2023-09-01 13:47:42 +0530

    bug

--- client/src/components/Layout/BookMarks.jsx
+++ client/src/components/Layout/BookMarks.jsx
@@ -1,39 +1,9 @@
-import React, { useEffect, useState } from "react";
-import axios from "axios";
-import api from "../Api";
+import React from "react";
 const BookMarks = () => {
-  const [Articles, setArticles] = useState([]);
-  const bookmarkShow = () => {
-    axios
-      .post(api + "/show", {
-        withCredentials: true,
-      })
-      .then((data) => {
-        setArticles(data.data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  };
-  useEffect(() => {
-    bookmarkShow();
-    console.log(Articles);
-  }, []);
-
   return (
     <div className="container">
       <div className="news-section">
-        {Articles.map((data, index) => {
-          return (
-            <div key={index} className="news-card">
-              <img src={data.image} alt="News 1" />
-              <div className="news-card-content">
-                <h3>{data.title}</h3>
-                <p>{data.description}</p>
-              </div>
-            </div>
-          );
-        })}
+        
       </div>
     </div>
   );
