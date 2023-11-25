e950992c6ef933bb63ac94cad3d5352b415d901d
Dhanish2603 <dhanishpatel.200410107076@svitvasad.ac.in>
2023-11-25 23:23:52 +0530

    cors changes

--- server/index.js
+++ server/index.js
@@ -5,39 +5,39 @@ const routes = require("./routes/routes");
 const db = require("./model/db");
 const cors = require("cors");
 require("dotenv").config();
-const helmet = require("helmet");
+// const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const path = require("path");
 
 // middlewares
-app.use((req, res, next) => {
-  res.set({
-    "Access-Control-Allow-Origin":
-      "https://dailyinsight-app-client.netlify.app",
-    "Access-Control-Allow-Methods": "*",
-    "Access-Control-Allow-Headers":
-      "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
-  });
+// app.use((req, res, next) => {
+//   res.set({
+//     "Access-Control-Allow-Origin":
+//       "*",
+//     "Access-Control-Allow-Methods": "*",
+//     "Access-Control-Allow-Headers":
+//       "'Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token'",
+//   });
 
-  next();
-});
-app.use(express.static(path.join(__dirname, "build")));
+//   next();
+// });
+// app.use(express.static(path.join(__dirname, "build")));
 
-app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+// app.use(helmet());
+// app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
 // Handle other routes by serving the index.html file
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "build", "index.html"));
-});
+// app.get("*", (req, res) => {
+//   res.sendFile(path.join(__dirname, "build", "index.html"));
+// });
 app.use(
   cors({
-    origin: true,
+    origin: "*",
     methods: ["GET", "POST"],
     credentials: true,
   })
 );
 // app.use(helmet());
-app.use("/", express.static(__dirname + "/build"));
+// app.use("/", express.static(__dirname + "/build"));
 app.use(cookieParser());
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
